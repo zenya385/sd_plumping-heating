9a28e2425a88410edb744b4c5f61e579436d1392
Zeno Roman <zeno385@gmail.com>
2023-04-10 10:32:24 +0300

    +

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,14 +2,15 @@
   const mobileMenu = document.querySelector(".js-menu-container");
   const openMenuBtn = document.querySelector(".js-open-menu");
   const closeMenuBtn = document.querySelector(".js-close-menu");
-  const openSubMenuBtn = document.querySelector(".mob-submenu"); //*
+  // const openSubMenuBtn = document.querySelector(".mob-submenu"); //*
+  // const subMenuList = document.querySelector(".list"); //*
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
     mobileMenu.classList.toggle("is-open");
-   
+
     const scrollLockMethod = !isMenuOpen
       ? "disableBodyScroll"
       : "enableBodyScroll";
@@ -18,11 +19,16 @@
 
   openMenuBtn.addEventListener("click", toggleMenu);
   closeMenuBtn.addEventListener("click", toggleMenu);
-  openSubMenuBtn.addEventListener("click", () => {
-    
-    openSubMenuBtn.classList.toggle('expand')
-  })
-;
+  // document.addEventListener("click", (event) => {
+  //   const isClickInsideBtn = openSubMenuBtn.contains(event.target);
+  //   const isClickInsideList = subMenuList.contains(event.target);
+
+  //   if (isClickInsideBtn) {
+  //     openSubMenuBtn.classList.toggle("expand");
+  //   } else if (!isClickInsideList) {
+  //     openSubMenuBtn.classList.remove("expand");
+  //   }
+  // });
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia("(max-width: 992px)").addEventListener("change", (e) => {
--- src/js/select.js
+++ src/js/select.js
@@ -1,24 +1,40 @@
-// $(document).ready(function () {
-//   $("#boiler").select2({
-//     // placeholder: "Select",
-//     // allowClear: true,
-//   });
+
+
+// const selectFild = document.querySelector(".select-fild");
+// const selectText = document.querySelector(".select-text");
+// const options = document.getElementsByClassName("options");
+// const list = document.querySelector(".select-list");
+// const arrowIcon = document.querySelector(".arrow-icon");
+
+// selectFild.addEventListener("click", () => {
+//   list.classList.toggle("hide");
+//   arrowIcon.classList.toggle("rotate");
 // });
 
-const selectFild = document.querySelector(".select-fild");
-const selectText = document.querySelector(".select-text");
-const options = document.getElementsByClassName("options");
-const list = document.querySelector(".select-list");
-const arrowIcon = document.querySelector(".arrow-icon");
+// for (let option of options) {
+//   option.addEventListener("click", (event) => {
+//     selectText.innerHTML = event.target.textContent;
+//     list.classList.toggle("hide");
+//   });
+// }
 
-selectFild.addEventListener("click", () => {
-  list.classList.toggle("hide");
-  arrowIcon.classList.toggle("rotate");
-});
 
-for (let option of options) {
-  option.addEventListener("click", (event) => {
-    selectText.innerHTML = event.target.textContent;
-    list.classList.toggle("hide");
+const selectFilds = document.querySelectorAll(".select-fild");
+const selectTexts = document.querySelectorAll(".select-text");
+const optionLists = document.querySelectorAll(".select-list");
+const arrowIcons = document.querySelectorAll(".arrow-icon");
+
+for (let i = 0; i < selectFilds.length; i+=1) {
+  selectFilds[i].addEventListener("click", () => {
+    optionLists[i].classList.toggle("hide");
+    arrowIcons[i].classList.toggle("rotate");
   });
+
+  const options = optionLists[i].getElementsByClassName("options");
+  for (let option of options) {
+    option.addEventListener("click", (event) => {
+      selectTexts[i].innerHTML = event.target.textContent;
+      optionLists[i].classList.toggle("hide");
+    });
+  }
 }
